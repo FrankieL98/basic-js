e9cd9e3f8492067efccff643a270d783655e6cd8
Yanina Vrubleuskaya <varyblevskaya8@gmail.com>
2023-04-17 00:19:39 +0200

    add code

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,84 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  square = [];
+  constructor(type = true) {
+    this.type = type;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  generateSquare() {
+    for (let i = 0; i < this.alpha.length; i++) {
+      let row = [];
+      for (let r = i; r < this.alpha.length; r++) {
+        row.push(this.alpha[r]);
+      }
+      let j = 0;
+      while (row.length < this.alpha.length) {
+        row.push(this.alpha[j]);
+        j += 1;
+      }
+      this.square.push(row);
+    }
+    return this.square;
+  }
+
+  encrypt(message, key) {
+    if (message == undefined || key == undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+    this.generateSquare();
+    let result = [];
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let k = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
+        if (k >= key.length) {
+          k = 0;
+        }
+        result.push(
+          this.square[this.alpha.indexOf(message[i])][this.alpha.indexOf(key[k])]
+        );
+        k += 1;
+      } else {
+        result.push(message[i]);
+      }
+    }
+    if (this.type) {
+      return result.join('');
+    } else {
+      return result.reverse().join('');
+    }
+  }
+
+  decrypt(message, key) {
+    if (message == undefined || key == undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+    this.generateSquare();
+    let result = [];
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let k = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
+        if (k >= key.length) {
+          k = 0;
+        }
+        result.push(
+          this.alpha[this.square[this.alpha.indexOf(key[k])].indexOf(message[i])]
+        );
+        k += 1;
+      } else {
+        result.push(message[i]);
+      }
+    }
+    if (this.type) {
+      return result.join('');
+    } else {
+      return result.reverse().join('');
+    }
   }
 }
 
